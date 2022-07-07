18b56b4a439606c4daca1c51d9727a4cbace3571
Anna Kawasaki <annk2@pdx.edu>
2022-07-06 20:02:21 -0700

    Added divisible by 3 case

--- FooBarQix/src/main/java/edu/pdx/cs410J/TheBest/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/TheBest/Kata.java
@@ -8,9 +8,15 @@ package edu.pdx.cs410J.TheBest;
  */
 public class Kata {
                                                                                     
-
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
     System.exit(1);
+  }
+
+  public static String divisibleBy3(int number) {
+    if(number % 3 == 0) {
+      return "Foo";
+    }
+    return "notFoo";
   }
 }
--- FooBarQix/src/test/java/edu/pdx/cs410J/TheBest/KataTest.java
+++ FooBarQix/src/test/java/edu/pdx/cs410J/TheBest/KataTest.java
@@ -2,12 +2,22 @@ package edu.pdx.cs410J.TheBest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class KataTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new Kata();
+  }
+
+  @Test
+  void isDivisibleBy3DoesNotContain3(){
+    int number = 9;
+    Kata myKata = new Kata();
+    assertThat(myKata.divisibleBy3(number), is("Foo"));
   }
 
 }
